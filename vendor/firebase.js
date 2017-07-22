8a00a9e014b5d0f273855960a507a0f1ed1bef3c
rock <yazin@whitepayments.com>
2017-07-22 22:14:53 +0300

    Initial

---
+++ vendor/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD8jkWizbALS9dp0rv8deFKlu-yTWqACWk",
+  authDomain: "website-1500659616199.firebaseapp.com",
+  databaseURL: "https://website-1500659616199.firebaseio.com",
+  projectId: "website-1500659616199",
+  storageBucket: "",
+  messagingSenderId: "560573637378"
+};
+firebase.initializeApp(config);
